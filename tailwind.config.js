ffc26d77512b0bf2fe49b9c0babb9ae9132e124a
r3nanp <renanmol87@gmail.com>
2021-01-07 17:09:40 -0300

    feat: add tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'ui-sans-serif']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
