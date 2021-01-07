986cba46339a0f60d1f28b2df611137130749848
r3nanp <renanmol87@gmail.com>
2021-01-07 12:48:54 -0300

    feat: create dynamic ssg page

---
+++ src/pages/[slug].tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import PostLayout from '../_layouts/PostLayout'
+import { getPostBySlug, getAllPosts } from './api/post'
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next'
+
+interface PostProps {
+  title: string
+  description: string
+  thumbnailUrl: string
+  content: string
+}
+
+export default function Post(props: PostProps) {
+  return (
+    <PostLayout
+      title={props.title}
+      description={props.description}
+      thumbnailUrl={props.thumbnailUrl}
+      content={props.content}
+    />
+  )
+}
+
+export const getStaticProps: GetStaticProps = async (
+  context: GetStaticPropsContext
+) => {
+  return {
+    props: await getPostBySlug(context.params.slug),
+  }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  let paths = await getAllPosts()
+
+  paths = paths.map(post => {
+    return {
+      params: { slug: post.slug },
+    }
+  })
+
+  return {
+    paths: paths,
+    fallback: false,
+  }
+}
