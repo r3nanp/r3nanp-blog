7f1793cba59281b988db9f53eba0d635221c184f
r3nanp <renanmol87@gmail.com>
2021-01-08 14:06:39 -0300

    feat: create puppeteer configuration

---
+++ src/pages/api/_lib/chromium.ts
@@ -0,0 +1,30 @@
+import puppeteer, { Page } from 'puppeteer-core'
+import { getOptions } from './chromiumOptions'
+
+let _page: Page | null
+
+async function getPage(isDev: boolean): Promise<Page> {
+  if (_page) {
+    return _page
+  }
+
+  const options = await getOptions(isDev)
+  const browser = await puppeteer.launch(options)
+
+  _page = await browser.newPage()
+
+  return _page
+}
+
+export async function getScreenshot(
+  isDev: boolean,
+  html: string
+): Promise<Buffer> {
+  const page = await getPage(isDev)
+
+  await page.setViewport({ width: 1200, height: 630 })
+  await page.setContent(html)
+
+  const file = await page.screenshot({ type: 'png' })
+  return file
+}
