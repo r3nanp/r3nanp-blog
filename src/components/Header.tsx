e143d2b309c359446d69b757f88da7cf206e1e10
r3nanp <renanmol87@gmail.com>
2021-01-12 16:23:00 -0300

    style(Header): add hamburger menu and change styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import Link from 'next/link'
 
+import { GiHamburgerMenu } from 'react-icons/gi'
+
 const Header: React.FC = () => {
   const [toggle, setToggle] = useState(false)
 
@@ -11,15 +13,9 @@ const Header: React.FC = () => {
       <div className="block md:hidden">
         <button
           onClick={() => setToggle(!toggle)}
-          className="md:hidden lg:hidden flex items-center justify-between px-3 py-2 border rounded text-white border-teal-400 hover:border-white"
+          className="md:hidden lg:hidden flex items-center justify-between px-3 py-2 rounded text-white hover:border-white"
         >
-          <svg
-            className="fill-current h-3 w-3 "
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
+          <GiHamburgerMenu size={20} color="#000000" />
         </button>
       </div>
       <div
