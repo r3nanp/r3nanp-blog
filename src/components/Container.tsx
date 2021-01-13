55c32e63f430017131a00c1be1a0ec9643ce4294
r3nanp <renanmol87@gmail.com>
2021-01-13 16:09:27 -0300

    feat: create Container component

---
+++ src/components/Container.tsx
@@ -0,0 +1,16 @@
+import { motion } from 'framer-motion'
+
+const Container: React.FC = ({ children }) => {
+  return (
+    <motion.main
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 1, duration: 1 }}
+      className="flex justify-center items-center"
+    >
+      {children}
+    </motion.main>
+  )
+}
+
+export { Container }
