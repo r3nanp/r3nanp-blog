7ed2820e7754329c460f436609ec787ba049fab2
r3nanp <renanmol87@gmail.com>
2021-01-14 16:20:02 -0300

    feat: create about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,28 +1,36 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import { Header } from 'components/Header'
+import { FaLinkedin, FaGithub } from 'react-icons/fa'
+import { AiOutlineMail } from 'react-icons/ai'
 
 const About: React.FC = () => {
   return (
-    <main className="h-screen bg-gray-600">
+    <main className="h-screen">
       <Head>
         <title>Sobre mim</title>
       </Head>
       <Header />
 
-      <div className="bg-gray-400">
+      <div className="px-4 border-b border-black h-1/2 flex flex-col items-center justify-center">
         <h1 className="text-2xl font-bold text-black text-center">Sobre mim</h1>
         <p className="text-md text-black text-center">Conheça-me!</p>
       </div>
 
-      <div className="bg-gray-800">
-        <h2 className="text-xl font-bold">Sobre mim</h2>
-        <p className="leading-4 tracking-wide text-black text-justify">
+      <div className="px-4 py-6">
+        <h2 className="text-xl font-bold text-white">Sobre mim</h2>
+        <p className="leading-8 tracking-wide text-black pb-2 text-center md:text-justify">
           Olá, meu nome é Renan Pereira, sou desenvolvedor de software. Nasci em
           Fortaleza, CE, sou apaixonado por programação e seu ecossistema!
         </p>
+        <p className="leading-8 tracking-wide text-black text-center md:text-justify">
+          Sou técnico em Redes de computadores e desde 2018 estudo programação.
+          Comecei com PHP e Python, e hoje estou trabalhando com Javascript,
+          Typescript, React e Node.
+        </p>
 
-        <h2 className="text-xl font-bold">Habilidades</h2>
-        <ul className="list-disc px-2">
+        <h2 className="text-xl font-bold py-2">Habilidades</h2>
+        <ul className="px-2">
           <li>HTML</li>
           <li>CSS</li>
           <li>Javascript</li>
@@ -36,30 +44,34 @@ const About: React.FC = () => {
           <li>Bancos de dados (MySQL, PostgreSQL, MongoDB)</li>
         </ul>
 
-        <h2 className="text-xl font-bold">Contato</h2>
-        <ul className="list-disc px-2">
-          <li>
-            <a
-              href="https://linkedin.com/in/r3nanp"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              LinkedIn
+        <h2 className="text-xl font-bold py-2 mb-4">Contato</h2>
+        <div className="flex flex-col w-full justify-center items-center md:items-stretch md:justify-start">
+          <Link href="https://linkedin.com/in/r3nanp">
+            <a className="mb-2" rel="noopener noreferrer" target="_blank">
+              <div className="flex items-center justify-center w-48 rounded text-white bg-blue-800 p-4 hover:bg-blue-900">
+                <FaLinkedin size={20} color="#fff" />
+                <p className="px-2">LinkedIn</p>
+              </div>
             </a>
-          </li>
-          <li>
-            <a
-              href="https://github.com/r3nanp"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              Github
+          </Link>
+          <Link href="https://github.com/r3nanp">
+            <a className="mb-2" rel="noopener noreferrer" target="_blank">
+              <div className="flex items-center justify-center w-48 rounded text-white bg-gray-800 p-4 hover:bg-gray-900">
+                <FaGithub size={20} color="#fff" />
+                <p className="px-2">Github</p>
+              </div>
             </a>
-          </li>
-          <li>
-            <a href="mailto:renanmol87@gmail.com">Email</a>
-          </li>
-        </ul>
+          </Link>
+
+          <Link href="mailto:renanmol87@gmail.com">
+            <a rel="noopener noreferrer" target="_blank">
+              <div className="flex items-center justify-center w-48 rounded text-white bg-red-700 p-4 hover:bg-red-900">
+                <AiOutlineMail size={20} color="#fff" />
+                <p className="px-2">Email</p>
+              </div>
+            </a>
+          </Link>
+        </div>
       </div>
     </main>
   )
