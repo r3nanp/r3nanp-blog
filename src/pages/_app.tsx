16399e5cec3c73293a99392515b43e494985b7ca
r3nanp <renanmol87@gmail.com>
2021-01-16 10:54:26 -0300

    refactor(fix): add linter support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import '../styles/tailwind.css'
 import '../styles/global.css'
+// eslint-disable-next-line no-use-before-define
+import React from 'react'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
