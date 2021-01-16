c31d3b20f4717234f21cb3242b5370e8c72f85d2
r3nanp <renanmol87@gmail.com>
2021-01-16 00:50:31 -0300

    feat: add meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line no-use-before-define
+import React from 'react'
 import Document, {
   DocumentContext,
   DocumentInitialProps,
@@ -15,14 +17,20 @@ export default class MyDocument extends Document {
     return { ...initialProps }
   }
 
+  // eslint-disable-next-line no-undef
   render(): JSX.Element {
     return (
       <Html>
         <Head>
           <meta charSet="utf-8" />
+          <meta name="theme-color" content="#e1e1e6" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap"
             rel="stylesheet"
           />
         </Head>
