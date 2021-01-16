b80f747f08ea0007e6a3ba60fe3a2065541c2403
r3nanp <renanmol87@gmail.com>
2021-01-16 00:51:24 -0300

    feat: add types and favico

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
+// eslint-disable-next-line no-use-before-define
+import React from 'react'
 import { GetStaticProps } from 'next'
 import { getAllPosts } from './api/post'
 import Link from 'next/link'
 import Head from 'next/head'
 
 import { motion } from 'framer-motion'
-import { Header } from 'components/Header'
-import { Container } from 'components/Container'
+import { Header } from '../components/Header'
+import { Container } from '../components/Container'
 
 interface HomeProps {
   posts: Array<{
@@ -14,19 +16,23 @@ interface HomeProps {
   }>
 }
 
-export default function Home(props: HomeProps) {
+const Home: React.FC<HomeProps> = ({ posts }) => {
   return (
-    <main className="bg-blue-800 h-screen w-full">
+    <main className="bg-white h-screen w-full">
       <Head>
-        <title>Bem vindo!</title>
+        <link rel="icon" href="/favicon.ico" />
+        <title>Renan Pereira - Desenvolvedor de software</title>
       </Head>
 
       <Header />
       <Container>
         <ul className="py-2 grid grid-cols-1 grid-rows-4 gap-y-4 sm:px-6 md:px-8 md:grid-cols-3 md:grid-rows-1 gap-x-2">
-          {props.posts.map((post, index) => (
-            <li key={index} className="flex justify-center md:justify-start items-center w-min">
-              <Link href={post.slug}>
+          {posts.map((post, index) => (
+            <li
+              key={index}
+              className="flex justify-center md:justify-start items-center w-min"
+            >
+              <Link href={`/${post.slug}`}>
                 <a>
                   <motion.div
                     whileHover={{ scale: 1.1 }}
@@ -55,3 +61,5 @@ export const getStaticProps: GetStaticProps = async () => {
     },
   }
 }
+
+export default Home
