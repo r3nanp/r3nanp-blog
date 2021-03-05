39916e90336cef8b9a9950bf747852f66fc0632b
r3nanp <renanmol87@gmail.com>
2021-03-04 22:32:12 -0300

    refactor(document): add global icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,3 @@
-// eslint-disable-next-line no-use-before-define
-import React from 'react'
 import Document, {
   DocumentContext,
   DocumentInitialProps,
@@ -17,13 +15,13 @@ export default class MyDocument extends Document {
     return { ...initialProps }
   }
 
-  // eslint-disable-next-line no-undef
   render(): JSX.Element {
     return (
       <Html>
         <Head>
           <meta charSet="utf-8" />
           <meta name="theme-color" content="#e1e1e6" />
+          <link rel="icon" href="/icon.png" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
@@ -33,6 +31,11 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap"
             rel="stylesheet"
           />
+          <script
+            data-ad-client="ca-pub-5125791754395683"
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          ></script>
         </Head>
 
         <body>
