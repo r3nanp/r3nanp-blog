39c660cbd32c842b314f4a70d7952fa041e37f3f
r3nanp <renanmol87@gmail.com>
2021-03-06 19:53:59 -0300

    feat: add service worker

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,22 @@
 import '../styles/tailwind.css'
 import '../styles/global.css'
 import { AppProps } from 'next/app'
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/serviceworker.js')
+        .then(() => {
+          console.log('service worker registered')
+        })
+        .catch(() => {
+          console.warn('service worker failed')
+        })
+    }
+  }, [])
+
   return <Component {...pageProps} />
 }
 
